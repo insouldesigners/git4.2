f2c2e1a4de387fc141c081496d23f34ad8617ac7
Katarina Sinister Blade <iorikuni69@gmail.com>
2022-10-28 22:35:11 +0700

    added plus

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            double firstValue, secondValue;
+            string action;
+
+            Console.WriteLine("Введите A");
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите B");
+            secondValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию '+' '-' '*' '/'");
+            action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "+":
+                    Console.WriteLine(firstValue + secondValue);
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
