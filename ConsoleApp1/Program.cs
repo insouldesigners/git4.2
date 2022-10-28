5d32b1708fe5d73e6c360760f22dac6ab37ba405
Katarina Sinister Blade <iorikuni69@gmail.com>
2022-10-28 22:36:38 +0700

    added minus

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -28,6 +28,9 @@ namespace ConsoleApp1
                 case "+":
                     Console.WriteLine(firstValue + secondValue);
                     break;
+                case "-":
+                    Console.WriteLine(firstValue - secondValue);
+                    break;
             }
             Console.ReadLine();
         }
